fd754f967d7ecd163867254292072a32214cca86
Tom England <tomengland6679@gmail.com>
2020-10-08 00:27:59 +0100

    Added selector of random word

--- hangman.c
+++ hangman.c
@@ -1,2 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+int main(){
+	// Seed the random number generator with current time
+	srand(time(0));	
+	
+	// Declare words
+	int wordCount = 2;
+	const char *words[wordCount];
+	words[0] = "test";
+	words[1] = "word";
+	
+	// Select word to guess
+	int selectedWordIndex = rand() % wordCount;
+	printf("Selected Word: %s \n", words[selectedWordIndex]);
+	
+	printf("Test");
+
+	return 0;
+}
