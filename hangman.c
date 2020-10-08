9fa93abbb0b5331c9a199c022abd2e0e817b493c
Tom England <tomengland6679@gmail.com>
2020-10-08 01:08:01 +0100

    Added a function to check if a char exists in a char array and added some guess handling

--- hangman.c
+++ hangman.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
+
+bool inArray(char *arr, char *c){
+	for (int i = 0; i < sizeof(&arr)/sizeof(arr[0]); ++i){
+		if(arr[i] == c) return true;
+	}
+	return false;
+}
 
 int main(){
 	// Seed the random number generator with current time
 	srand(time(0));	
 	
+	int lives = 5;
+	char guess;
+	char previousGuesses[26];
+
 	// Declare words
 	int wordCount = 2;
 	const char *words[wordCount];
@@ -16,7 +28,16 @@ int main(){
 	int selectedWordIndex = rand() % wordCount;
 	printf("Selected Word: %s \n", words[selectedWordIndex]);
 	
-	printf("Test");
+	while (lives > 0){
+		printf("Lives: %d \n", lives);
+		printf("Enter Guess: ");
+		scanf("%c", &guess);
+
+		if (inArray(words[selectedWordIndex], guess)) {
+			printf("Guess is in word \n");
+		}
+		lives--;
+	}
 
 	return 0;
 }
